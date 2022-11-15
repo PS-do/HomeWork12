0db38d9ba8f71f1d766f31f9151929d1f4119f71
Сергей Плюхин <psg.pgs@mail.ru>
2022-11-16 01:17:12 +0300

    Добавьте файлы проекта.

---
+++ HomeWork12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задачи\n" +
+                "1.   Разработать статический класс для работы с окружностью. Класс должен содержать 3 метода:" +
+                "\r\nметод, определяющий длину окружности по заданному радиусу;" +
+                "\r\nметод, определяющий площадь круга по заданному радиусу;" +
+                "\r\nметод, проверяющий принадлежность точки с координатами (x,y) кругу с радиусом r и координатами центра x0, y0.\r\n");
+            Console.Write("r=");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.Write("x=");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y=");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"l={Round.Get_l(r)}");
+            Console.WriteLine($"l={Round.Get_s(r)}");
+            if (Round.Get_isIn(r, x, y)) Console.WriteLine("Точка в круге");
+            else Console.Write("точка за кругом");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork12/Round.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork12
+{
+    static class Round
+    {
+        static public double Get_l(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double Get_s(double r)
+        {
+            return Math.PI * r * r;
+        }
+        static public bool Get_isIn(double r, double x,double y)
+        {
+            double l = Math.Sqrt(x*x+y*y);
+            if (l>r) return false;
+            else return true;
+        }
+
+    }
+}
